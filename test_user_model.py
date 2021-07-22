f3340b334d05b9a459cdca3c9a933bc83dbbe752
stocktons <sarah@thanksforallthe.fish>
2021-07-22 14:12:16 -0400

    First three tests

--- test_user_model.py
+++ test_user_model.py
@@ -5,7 +5,9 @@
 #    python -m unittest test_user_model.py
 
 
+from inspect import unwrap
 import os
+from re import U # what is this?
 from unittest import TestCase
 
 from models import db, User, Message, Follows
@@ -27,6 +29,17 @@ from app import app
 
 db.create_all()
 
+USER1_DATA = {
+    "email" : "test@test.com",
+    "username" : "testuser",
+    "password" : "HASHED_PASSWORD"
+}
+
+USER2_DATA = {
+    "email" : "test2@test.com",
+    "username" : "testuser2",
+    "password" : "HASHED_PASSWORD"
+}
 
 class UserModelTestCase(TestCase):
     """Test views for messages."""
@@ -38,20 +51,48 @@ class UserModelTestCase(TestCase):
         Message.query.delete()
         Follows.query.delete()
 
+        user1 = User(**USER1_DATA)
+        user2 = User(**USER2_DATA)
+        db.session.add_all([user1, user2])
+        db.session.commit()
+
         self.client = app.test_client()
+        self.user1 = user1
+        self.user2 = user2
 
     def test_user_model(self):
         """Does basic model work?"""
 
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u)
-        db.session.commit()
+        u = self.user1
 
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+
+    def test_user_repr(self):
+        """Does the repr work?"""
+
+        u = self.user1
+        test_user_id = u.query.get(u.id)
+
+        self.assertEqual(f"{test_user_id}", f"<User #{u.id}: {u.username}, {u.email}>")
+
+    def test_is_following(self):
+        """Is user1 (Fran) following user2 (Bob)?"""
+
+        u1 = self.user1
+        u2 = self.user2
+
+        u1.following.append(u2)
+        db.session.commit()
+
+        self.assertEqual(u1.is_following(u2), True)
+
+    def test_is_not_following(self):
+        """Is user1 NOT following user2?"""
+
+        u1 = self.user1
+        u2 = self.user2
+
+        self.assertEqual(u1.is_following(u2), False)
